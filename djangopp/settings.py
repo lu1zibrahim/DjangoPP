c5fb33f7901f3c85689ccde4e0d8b65637cc7fa5
lu1zibrahim <luizibrahim94@gmail.com>
2021-11-14 12:31:53 -0300

    Criação de variaveis locais e em produção.

--- djangopp/settings.py
+++ djangopp/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+from decouple import config
 
 from pathlib import Path
 
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i9%q+t&rws0577g2@04wup@fe!2eu*o_=0ngv81+p4@d_yl=i&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
