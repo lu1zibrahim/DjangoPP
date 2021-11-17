0e96c7d093b3829758ec9eb16b312ae1820e4113
lu1zibrahim <luizibrahim94@gmail.com>
2021-11-17 07:18:47 -0300

    Comentada as ferramentas para dar "trigger" no sentry

--- djangopp/urls.py
+++ djangopp/urls.py
@@ -17,13 +17,17 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-from base.views import home, trigger_error
+from base.views import home
+
+# from base.views import trigger_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('sentry-debug/', trigger_error),
+
 ]
+
+# urlpatterns.append(path('sentry-debug/', trigger_error))
 
 if settings.DEBUG:
     import debug_toolbar
