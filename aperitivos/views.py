4bb7bcef479919c538813b75857d57c6c4398663
lu1zibrahim <luizibrahim94@gmail.com>
2021-11-23 08:38:50 -0300

    Adicionado página de listagem dos vídeos.

--- aperitivos/views.py
+++ aperitivos/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
 
+videos = [
+    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 647338996},
+    {'slug': 'impressao', 'titulo': 'Impressão 3d', 'vimeo_id': 648966504},
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
+
 def video(request, slug):
-    videos = {
-        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 647338996},
-        'impressao': {'titulo': 'Impressão 3d', 'vimeo_id': 648966504},
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
