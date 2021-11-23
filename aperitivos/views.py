0de77dd4efe43726466c322b5d1914970bb8906e
lu1zibrahim <luizibrahim94@gmail.com>
2021-11-23 08:44:32 -0300

    Transformado o dicionario para os vídeos em uma classe.

--- aperitivos/views.py
+++ aperitivos/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
 
+class Video:
+    def __init__(self, slug, titulo, vimeo_id):
+        self.vimeo_id = vimeo_id
+        self.titulo = titulo
+        self.slug = slug
+
+
 videos = [
-    {'slug': 'motivacao', 'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 647338996},
-    {'slug': 'impressao', 'titulo': 'Impressão 3d', 'vimeo_id': 648966504},
+    Video('motivacao', 'Video Aperitivo: Motivação', 647338996),
+    Video('impressao', 'Impressão 3d', 648966504)
 ]
 
-videos_dct = {dct['slug']: dct for dct in videos}
+videos_dct = {v.slug: v for v in videos}
 
 
 def indice(request):
