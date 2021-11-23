a5329953f6a29d07ad2ae5592bb8173ee2f313f5
lu1zibrahim <luizibrahim94@gmail.com>
2021-11-23 07:11:06 -0300

    Adicionado a lógica para implementação de slug hardcoded

--- aperitivos/views.py
+++ aperitivos/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos={
+        'motivacao': {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 647338996},
+        'impressao': {'titulo': 'Impressão 3d', 'vimeo_id': 648966504},
+    }
+    video = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
